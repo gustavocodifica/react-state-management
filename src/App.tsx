e9494133d0a37508bbbdf7e376997da3ec89a55f
gustavocodifica <gustavo@codifica.dev>
2024-06-04 14:56:22 -0300

    chore: add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import "./App.css";
-
 export default function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-zinc-800">
+      <div className="max-w-3xl w-full flex mx-auto">
+        <h1>Todo app</h1>
+      </div>
+    </div>
+  );
 }
